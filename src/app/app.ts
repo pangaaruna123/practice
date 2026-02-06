9f7720bc06be4f5c8efd1fc24394e76992673cf1
Aruna Panga <160154257+pangaaruna123@users.noreply.github.com>
2026-02-06 06:27:33 +0000

    login changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { Webpage } from './webpage/webpage';
 import { Network } from './network';
 import { HttpClientModule } from '@angular/common/http';
+import { Login } from './login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [Webpage, CommonModule, HttpClientModule],
+  imports: [Login, CommonModule, HttpClientModule],
   providers: [Network],
   templateUrl: './app.html',
   styleUrl: './app.scss'
