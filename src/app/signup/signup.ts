3ef8fbeff5d4bc8db98b7bd7c417f227eeb92fcd
Aruna Panga <160154257+pangaaruna123@users.noreply.github.com>
2026-05-12 09:55:08 +0000

    signup creation

--- src/app/signup/signup.ts
+++ src/app/signup/signup.ts
@@ -1,9 +1,69 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidationErrors, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './signup.html',
   styleUrl: './signup.scss',
 })
-export class SignUp {}
+export class SignUp {
+  signupForm!: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.signupForm = this.fb.group({
+      username: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3)
+        ]
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6)
+        ]
+      ],
+      confirmPassword: [
+        '',
+        Validators.required
+      ]
+    },
+      {
+        validators: this.passwordMatchValidator
+      });
+  }
+  passwordMatchValidator(
+    control: AbstractControl
+  ): ValidationErrors | null {
+    const password =
+      control.get('password')?.value;
+    const confirmPassword =
+      control.get('confirmPassword')?.value;
+    if (password !== confirmPassword) {
+      return {
+        passwordMismatch: true
+      };
+    }
+    return null;
+  }
+  signup() {
+
+    if (this.signupForm.valid) {
+
+      console.log(this.signupForm.value);
+
+    }
+
+  }
+
+}
