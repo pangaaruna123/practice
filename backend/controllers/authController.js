f20c7448e474a3bb877c6588cc9eec9a70d34285
Aruna Panga <160154257+pangaaruna123@users.noreply.github.com>
2026-05-13 09:44:00 +0000

    backend implementation

---
+++ backend/controllers/authController.js
@@ -0,0 +1,81 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+
+// SIGNUP
+const signup = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    // Check existing user
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    // Encrypt password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create user
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "Signup successful",
+      user,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+
+// LOGIN
+const login = async (req, res) => {
+  try {
+
+    const { email, password } = req.body;
+
+    // Check user
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "User not found",
+      });
+    }
+
+    // Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid password",
+      });
+    }
+
+    res.status(200).json({
+      message: "Login successful",
+      user,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  signup,
+  login,
+};
---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+
+// Middlewares
+app.use(cors());
+
+app.use(express.json());
+
+
+// Routes
+app.use("/api/auth", require("./routes/authRoutes"));
+
+
+// Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
