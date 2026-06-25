7227e3c1dfa56dd75a3a3d0906b62eaad929eb61
Aruna Panga <160154257+pangaaruna123@users.noreply.github.com>
2026-06-25 08:52:51 +0000

    select seats based on user

--- src/app/network.ts
+++ src/app/network.ts
@@ -6,9 +6,9 @@ import { catchError, map, Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class Network {
-  userName:string='';
-      // apiurl="http://localhost:3000";
-    apiurl="https://miniature-space-potato-jj9w45rg7w6cpp55-3000.app.github.dev"
+  userName: string = '';
+  // apiurl="http://localhost:3000";
+  apiurl = "https://miniature-space-potato-jj9w45rg7w6cpp55-3000.app.github.dev"
   constructor(private http: HttpClient) { }
   getdata(payload: any): Observable<any> {
     return this.http.post(`${this.apiurl}/api/data`, payload).pipe(
@@ -37,7 +37,7 @@ export class Network {
     );
   }
   sendUserData(userData: any): Observable<any> {
-   return this.http.post(`${this.apiurl}/api/auth/signup`, userData).pipe(
+    return this.http.post(`${this.apiurl}/api/auth/signup`, userData).pipe(
       map((res: any) => {
         console.log('Data received:', res);
         return res;
@@ -47,12 +47,6 @@ export class Network {
         throw err;
 
       })
-    ); 
-  }
-  getUser(user:any){
-this.userName=user;
-  }
-  sendUser(){
-    return this.userName;
+    );
   }
 }
--- src/app/selectmovieseat/selectmovieseat.ts
+++ src/app/selectmovieseat/selectmovieseat.ts
@@ -1,39 +1,89 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { Network } from '../network';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+export interface Seat {
+  user: string;
+  seats: string[];
+}
 @Component({
   selector: 'app-selectmovieseat',
-  imports: [CommonModule,FormsModule,ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './selectmovieseat.html',
   styleUrl: './selectmovieseat.scss',
 })
 export class Selectmovieseat {
-userName:string='';
-colidx:number=-1;
-rows=['A','B','C','D','E'];
-seats=[1,2,3,4,5,6,7,8];
-rowIdx: number=-1;
+  userName: string = '';
+  colidx: number = -1;
+  rows = ['A', 'B', 'C', 'D', 'E'];
+  seats = [1, 2, 3, 4, 5, 6, 7, 8];
+  rowIdx: number = -1;
+  seatIds: string[] = [];
+  userSeatsData: Seat[] = [];
 
-  constructor() { }
-ngOnInit(){
-  let user=localStorage.getItem('currentUser');
-  if(user){
-    this.userName=JSON.parse(user);
+  constructor(private nt: Network, private cdr: ChangeDetectorRef) { }
+  ngOnInit() {
+    let user = localStorage.getItem('currentUser');
+    if (user) {
+      this.userName = JSON.parse(user);
+    }
+    console.log(user, '8888')
+   
   }
-// let userData=localStorage.getItem('users');
+  ngAfterViewInit() {
+ let userData = localStorage.getItem('userSeatsData');
 
-// if(userData){
-//   const user = JSON.parse(userData);
-//   console.log('User data:', user);
-// }
-}
-isOccupied(i: number, r: number){
-this.colidx=i;
-this.rowIdx=r;
-console.log(i,r)
-}
+    if (userData) {
+      const user = JSON.parse(userData);
+      console.log('User data:', user);
+      user.seats.forEach((seatId: string) => {
+        const seatElement = document.getElementById(seatId) as HTMLElement;
+        console.log('Seat element:', seatElement, seatId);
+        if (seatElement) {
+          seatElement.className = "disabled";
+        }
+        
+      });
 
+    }
+  }
+
+  isOccupied(r: number, i: number) {
+    console.log(r, i, '123')
+    const seatId = `${this.rows[r]}${this.seats[i]}`;
+    const seatElement = document.getElementById(seatId) as HTMLElement | null;
+    console.log('Seat element:', seatElement, seatId);
+
+    if (seatElement) {
+      //  this.seatIds.push({user: this.userName, seats: [seatId]});
+      seatElement.className = "occupied";
+    }
+    if (!this.seatIds.includes(seatId)) {
+      this.seatIds.push(seatId);
+    }
+    if (this.userName) {
+      const userData = {
+        user: this.userName,
+        seats: this.seatIds
+      }
+      let idx = this.userSeatsData.findIndex((u: any) => u.user === this.userName);
+      if (idx === -1) {
+        this.userSeatsData.push(userData);
+      }
+      console.log('Seat IDs:', this.seatIds, userData);
+      localStorage.setItem('userSeatsData', JSON.stringify(userData));
+    }
+  }
+  // disabledSeats(row:string,seat:number):boolean{
+  //   const seatId = `${row}${seat}`;
+  //   let userData=localStorage.getItem('userSeatsData');
+  //   if(userData){
+  //     const user = JSON.parse(userData);
+  //     if(user.seats.includes(seatId)){
+  //       return true;
+  //     }
+  //   }
+  //   return false; 
+  // }
 
 }
